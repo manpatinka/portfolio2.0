ed02d69dc9caa39a1dfc0b43321c877b8ebd9bf2
Evgenia <evgeniayurkova@gmail.com>
2023-07-28 15:53:04 +0300

    create projects page files

---
+++ src/components/projects/index.js
@@ -0,0 +1,18 @@
+import './index.scss';
+import { Triangle } from 'react-loader-spinner';
+
+const Projects = () => {
+    return (
+        <>
+
+            <Triangle
+                color="#b90e75"
+                height={100}
+                width={100}
+                wrapperClass='loader'
+            />
+        </>
+    );
+}
+
+export default Projects;
