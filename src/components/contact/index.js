f9333c720fad630be2d350d1e20e5658b1dd246b
Evgenia <evgeniayurkova@gmail.com>
2023-08-01 16:09:52 +0300

    add contact page, improve ui

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,18 +1,140 @@
-import './index.scss';
-import { Triangle } from 'react-loader-spinner';
+import './index.scss'
+import { Triangle } from 'react-loader-spinner'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faFacebook, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons'
+
 
 const Contact = () => {
+    const form = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_8piv1rb', 'template_kwaswvr', form.current, '6ijRi64rg6PKmmfm9')
+        .then(
+            () => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+            },
+            () => {
+              alert('Failed to send the message, please try again')
+            }
+          )
+    };
+
+
     return (
         <>
+            <div className="contact-container">
+                <h1>Contact me</h1>
+                <div className="structure">
+                    <div className="left-side">
+                        <div class="description">
+                            <h3>Let's Work Together:</h3>
+                            <p>Whether you're a small business owner, a startup enthusiast, or anyone seeking to establish a strong online presence, I am here to help. I welcome the opportunity to discuss your project requirements and provide tailored solutions that align with your goals.
+                            </p></div>
+                        <div className="links">
+                            <ul>
+                                <li>
+                                    <a
+                                        href="https://www.linkedin.com/in/evgeniayurkova/"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faLinkedin}
+                                            className="anchor-icon"
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.facebook.com/evgenia.yurkova"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faFacebook}
+                                            className="anchor-icon"
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://github.com/manpatinka"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faGithub}
+                                            className="anchor-icon"
+                                        />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="mailto:eyurkv@gmail.com"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        <FontAwesomeIcon
+                                            icon={faGoogle}
+                                            className="anchor-icon"
+                                        />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="right-side">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <p>Send me email directly:</p>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
             <Triangle
                 color="#b90e75"
-                height={100}
-                width={100}
+                height={150}
+                width={150}
                 wrapperClass='loader'
             />
         </>
     );
 }
- 
+
 export default Contact;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,6 +3,9 @@ import Profile from '../../assets/images/profile.png';
 import Skills from '../skills';
 import Certification from '../../assets/certification/certification.pdf'
 import { Triangle } from 'react-loader-spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSuperpowers } from '@fortawesome/free-brands-svg-icons';
+import { faCircle, faCircleArrowDown, faCircleDot, faCircleExclamation, faCircleNodes } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 
@@ -12,32 +15,46 @@ const Home = () => {
 
   return (
     <>
-    <div className="container home-page">
-    <div className="certification-container">
-      <button onClick={openCertification}>See professional certification</button>
-    </div>
-      <div className="text-zone">
-        <h1>Hi! My name is
-          <span className='initial'>E</span>
-          vgenia
-          <span className="initial" id="y">Y</span>
-          urkova.
-        </h1>
-        <h2>I'm a web developer from Vilnius, Lithuania.</h2>
-        <Skills />
+      <div className="container home-page">
+        <div className="certification-container">
+          <button onClick={openCertification}>See professional certification</button>
+        </div>
+        <div className="text-zone">
+          <h1>Hi! My name is
+            <span className='initial'>E</span>
+            vgenia
+            <span className="initial" id="y">Y</span>
+            urkova.
+          </h1>
+          <h2>I'm a web developer from Vilnius, Lithuania.</h2>
+          <h4>I believe that understanding your unique needs and vision is the key to creating a successful project. I'm committed to open communication, attention to detail, and delivering results that exceed your expectations.</h4>
+          <div className="offer">
+            <ul className='services'>
+              <li><FontAwesomeIcon icon={faCircleNodes} color="#086d66" width={10} />&nbsp;  Full Website Development</li>
+              <li><FontAwesomeIcon icon={faCircleNodes} color="#086d66" width={10} />&nbsp;  Responsive Design for Mobile Devices</li>
+              <li><FontAwesomeIcon icon={faCircleNodes} color="#086d66" width={10} />&nbsp;  Custom Web Applications</li>
+            </ul>
+
+          </div>
+        </div>
+
+        <div className="right-side">
+          <div className="profile-pic">
+            <img src={Profile} alt="profile picture" />
+          </div>
+
+          <Skills />
+        </div>
+
+
       </div>
 
-      <div className="profile-pic">
-        <img src={Profile} alt="profile picture" />
-      </div>
-    </div>
-
-    <Triangle 
-      color="#b90e75"
-      height={100}
-      width={100}
-      wrapperClass='loader'
-    />
+      <Triangle
+        color="#b90e75"
+        height={150}
+        width={150}
+        wrapperClass='loader'
+      />
     </>
   );
 }
